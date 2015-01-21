254b8cdfbdcd52df1fd7d4ef86e9916c3dc32992
Zlatko G <zlatko.gantchev@gmail.com>
2015-01-20 19:56:15 -0500

    Update with most recent version of Advanced Search Queries

--- parentory-sql-queries.sql
+++ parentory-sql-queries.sql
@@ -79,21 +79,56 @@ HAVING 		Count(*) >= 2
 
 /****** focused-search & advanced-search query ******/
 
-SELECT 		wp_posts.ID 
-FROM 		wp_posts 
-INNER JOIN 	wp_postmeta 
-			ON wp_posts.ID = wp_postmeta.post_id 
-WHERE 		wp_posts.post_type IN ('school') AND 
-			wp_posts.post_status = 'publish' AND 
-			(
-				wp_posts.post_title LIKE '%word1%' OR 
-				wp_posts.post_title LIKE '%word2%' OR 
-				wp_postmeta.meta_value LIKE '%word1%' OR 
-				wp_postmeta.meta_value LIKE '%word2%'
-			)
+/* 
+1. Query for Address & Province */
+
+SELECT 		G.ID
+FROM 		(
+			SELECT 		wp_posts.ID 
+			FROM 		wp_posts 
+			INNER JOIN 	wp_postmeta 
+						ON wp_posts.ID = wp_postmeta.post_id 
+			WHERE 		wp_posts.post_type IN ('school') 
+					AND wp_posts.post_status = 'publish' 
+					/* only include if Province was selected */
+					AND (
+							wp_postmeta.meta_key = "school-province" AND
+							wp_postmeta.meta_value = "dropdown_value"
+						)
+					/* only include if Address was input */
+					OR (
+							wp_postmeta.meta_value LIKE '%address_word1%' OR 
+							wp_postmeta.meta_value LIKE '%address_word2%'
+						)
+			) AS G
+GROUP BY	G.ID
+HAVING 		Count(*) = $total_num_criteria
+
+
+/* 
+2. Query for range fields */
+
+SELECT		G.post_id
+FROM		(
+				SELECT wp_postmeta.post_id, wp_postmeta.meta_value
+				FROM wp_postmeta
+				WHERE wp_postmeta.meta_key
+				IN ('school-annual-tuition-min',  'school-annual-tuition-max')
+			) AS G
+GROUP BY	G.post_id
+HAVING		(13000 BETWEEN MIN(G.meta_value) AND MAX(G.meta_value)) 
+			OR (18000 BETWEEN MIN(G.meta_value) AND MAX(G.meta_value))
+
+
+				IN ('school-class-size-min',  'school-class-size-max')
+				IN ('school-grades-min',  'school-grades-max')
+
 UNION 
 
-/* -- tested and works -- */
+
+/* 
+3. Query for Taxonomy Terms */
+
 SELECT 		G.object_id
 FROM		(
 			SELECT 		wp_term_relationships.object_id
@@ -102,8 +137,7 @@ FROM		(
 						ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id 
 			INNER JOIN 	wp_terms 
 						ON wp_term_taxonomy.term_id = wp_terms.term_id 
-			WHERE 		wp_terms.name IN ('Archery', 'Badminton', 'Baseball', ...)
+			WHERE 		wp_terms.slug IN ('Archery', 'Badminton', 'Baseball', ...)
 			) as G
 GROUP BY	G.object_id
 HAVING 		Count(*) = [termsCount]
-/* ----------------------- */
